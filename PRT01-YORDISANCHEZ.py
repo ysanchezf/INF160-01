2a3dbb8654d395c99a5433d0a7114c52acb65233
ysanchezf <ys21-0834@unphu.edu.do>
2023-05-12 11:07:01 -0400

    BRANCH

--- PRT01-YORDISANCHEZ.py
+++ PRT01-YORDISANCHEZ.py
@@ -10,4 +10,4 @@ sresultado = N1elevado2 + N1elevado2 #
 rresultado = "{:.4f}".format(sresultado ** (1/2)) # Utilizamos "4f" para que podamos decidir la cantidad de decimales que queremos mostrar luego del punto.
 
 # Asignamos la formula y imprimos el resultado final de la operacion.
-print ("√(" + str(numero1) + "²" + " + " + str(numero2) + ")²" +" = " + "√("+ str(N1elevado1) + " + " + str(N1elevado2)  + ") = " + "√("+ str(sresultado) + ") = " + str(rresultado)   )
+print ("√(" + str(numero1) + "²" + " + " + str(numero2) + ")²" +" = " + "√("+ str(N1elevado1) + " + " + str(N1elevado2)  + ") = " + "√("+ str(sresultado) + ") = " + str(rresultado))
